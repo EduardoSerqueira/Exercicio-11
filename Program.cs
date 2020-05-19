9030892f0c212e995793c15c86c975958350c237
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-18 22:23:39 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercicio_onze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B;
+
+            Console.WriteLine("Digite um valor: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor: ");
+            B = int.Parse(Console.ReadLine());
+
+            if (A % B == 0 || B % A == 0)
+            {
+                Console.WriteLine("São multiplos");
+            }
+            else
+            {
+                Console.WriteLine("Não são multiplos");
+            }
+            Console.ReadKey();
+        }
+    }
+}
